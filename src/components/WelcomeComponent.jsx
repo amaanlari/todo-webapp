f46b6dd445d2495ac446645af885769678e6126b
amaanlari <amaanlari@outlook.com>
2024-03-23 14:36:03 +0530

    connected react with hello world rest api using axios

--- src/components/WelcomeComponent.jsx
+++ src/components/WelcomeComponent.jsx
@@ -1,8 +1,44 @@
 import '../App.css';
 import { Link, useParams } from 'react-router-dom';
+import { useState } from 'react';
+import { retrieveHelloWorldBeanPathVariable } from '../api/HelloWorldApiService';
 
 function WelcomeComponent() {
   const { username } = useParams();
+  const [ message, setMessage ] = useState(null);
+
+  function callHelloWorldRestApi() {
+    console.log('called');
+
+    /*
+    retrieveHelloWorldBean()
+      .then((response) => successfulResponse(response))
+      .catch((error) => errorResponse(error))
+      .finally(() => console.log('clean up'));
+    */
+    
+    /*
+    retrieveHelloWorldBean()
+      .then((response) => successfulResponse(response))
+      .catch((error) => errorResponse(error))
+      .finally(() => console.log('clean up'));
+    */
+
+    retrieveHelloWorldBeanPathVariable('amaanlari')
+      .then((response) => successfulResponse(response))
+      .catch((error) => errorResponse(error))
+      .finally(() => console.log('clean up'));
+  }
+
+  function successfulResponse(response) {
+    console.log(response);
+    setMessage(response.data.message);
+  }
+
+  function errorResponse(error) {
+    console.log(error);
+    // setMessage(error.response.data);
+  }
 
   return (
     <div className="WelcomeComponent">
@@ -11,8 +47,16 @@ function WelcomeComponent() {
       <div>
         Manage your todos:{' '}
         <Link to={'/todo'}>
-          <button>Click here</button>
+          <button className='btn btn-dark'>Click here</button>
         </Link>
+      </div>
+      <div>
+        <button className="btn btn-success m-5" onClick={callHelloWorldRestApi}>
+          Call hello world Rest API.
+        </button>
+      </div>
+      <div className='text-info'>
+        <h4>{message}</h4>
       </div>
     </div>
   );
