f46a58b650377c44f6d0d5c1e78297d3a0b8909d
amaanlari <amaanlari@outlook.com>
2024-06-01 08:09:12 +0530

    updated dummy user credentials

--- src/components/WelcomeComponent.jsx
+++ src/components/WelcomeComponent.jsx
@@ -43,8 +43,8 @@ function WelcomeComponent() {
   }
 
   return (
-    <div className="WelcomeComponent container">
-      <div className="text-center">
+    <div className="WelcomeComponent container contain">
+      <div className='text-center'>
         <h1>ToDo Management Application</h1>
         <h2>Welcome {username}</h2>
         <div>
@@ -61,39 +61,43 @@ function WelcomeComponent() {
             Click for greeting.
           </button>
         </div>
-        <div>
-          <h3>Task Manager Application</h3>
-          <p>
-            Welcome to the Task Manager Application! This application allows you to manage your tasks efficiently and stay organized. With its user-friendly interface and powerful features, you can easily create, update, and delete tasks based on your needs.
-          </p>
-          <p>
-            Features:
-          </p>
-          <ul>
-            <li>Create new tasks</li>
-            <li>Update existing tasks</li>
-            <li>Delete tasks</li>
-            <li>Mark tasks as completed</li>
-            <li>Filter tasks based on status</li>
-          </ul>
-          <p>
-            Technology Used:
-          </p>
-          <ul>
-            <li>React.js - A popular JavaScript library for building user interfaces</li>
-            <li>React Router - A routing library for React applications</li>
-            <li>React Context API - A state management solution for React applications</li>
-            <li>Axios - A promise-based HTTP client for making API requests</li>
-            <li>RESTful API - Backend API for handling task management operations</li>
-          </ul>
-          <h3>
-            To get started, you can use the following dummy credentials:
-          </h3>
-          <ul>
-            <li>Username: dummy</li>
-            <li>Password: dummy</li>
-          </ul>
-        </div>
+      </div>
+      <div className='readme-container'>
+        <h3 className='text-center'>Task Manager Application</h3>
+        <p>
+          Welcome to the Task Manager Application! This application allows you
+          to manage your tasks efficiently and stay organized. With its
+          user-friendly interface and powerful features, you can easily create,
+          update, and delete tasks based on your needs.
+        </p>
+        <p>Features:</p>
+        <ul>
+          <li>Create new tasks</li>
+          <li>Update existing tasks</li>
+          <li>Delete tasks</li>
+          <li>Mark tasks as completed</li>
+          <li>Filter tasks based on status</li>
+        </ul>
+        <p>Technology Used:</p>
+        <ul>
+          <li>
+            React.js - A popular JavaScript library for building user interfaces
+          </li>
+          <li>React Router - A routing library for React applications</li>
+          <li>
+            React Context API - A state management solution for React
+            applications
+          </li>
+          <li>Axios - A promise-based HTTP client for making API requests</li>
+          <li>
+            RESTful API - Backend API for handling task management operations
+          </li>
+        </ul>
+        <h3>To get started, you can use the following dummy credentials:</h3>
+        <ul>
+          <li>Username: dummy</li>
+          <li>Password: dummy</li>
+        </ul>
       </div>
     </div>
   );
